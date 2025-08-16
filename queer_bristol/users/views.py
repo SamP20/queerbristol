2686ce6ca21f90d6d181c1a9ab0e58196fa15275
SamP20 <633856+SamP20@users.noreply.github.com>
2025-08-16 12:25:55 +0200

    User admin

--- queer_bristol/users/views.py
+++ queer_bristol/users/views.py
@@ -1,14 +1,23 @@
 
 
-from flask import Blueprint, redirect, render_template, request, url_for
+from flask import Blueprint, abort, flash, g, redirect, render_template, request, url_for
 import sqlalchemy as sa
 
+from queer_bristol.forms import DeleteConfirmForm
+from queer_bristol.login import login_required
 from queer_bristol.models import Group, User
 from queer_bristol.extensions import db
 from queer_bristol.users.forms import UserForm
 
 
 bp = Blueprint("users", __name__, url_prefix="/users")
+
+@bp.before_request
+@login_required
+def before_request():
+    """ Protect all user admin endpoints. """
+    if not g.user.admin:
+        abort(403)
 
 def filter_request_args(filter: set[str]):
     return {k: v for k, v in request.args.items() if k in filter}
@@ -21,10 +30,17 @@ def index():
 
     query_args = filter_request_args({"search"})
 
-    prev_url = url_for('.index', page=users.prev_num, **query_args) if users.has_prev else None
-    next_url = url_for('.index', page=users.next_num, **query_args) if users.has_next else None
+    prev_url = url_for('.index', page=users.prev_num, **query_args) if users.has_prev else None # type: ignore
+    next_url = url_for('.index', page=users.next_num, **query_args) if users.has_next else None # type: ignore
 
     return render_template("users/index.html", users=users, prev_url=prev_url, next_url=next_url)
+
+@bp.route("/<int:user_id>")
+def user(user_id: int):
+    user = db.get_or_404(User, user_id)
+
+    return render_template("users/user.html", user=user)
+
 
 @bp.route("/<int:user_id>/edit", methods=["GET", "POST"])
 def edit(user_id):
@@ -42,14 +58,60 @@ def edit(user_id):
     groups_by_id = {g.id: g for g in groups}
 
     if form.validate_on_submit():
-        user.name = form.name.data
-        user.email = form.email.data
+        user.name = form.name.data or ""
+        user.email = form.email.data or ""
         user.admin = form.admin.data
         user.helper = form.helper.data
-        user.groups = [groups_by_id[gid] for gid in form.groups.data]
+        groups = form.groups.data or []
+        user.groups = [groups_by_id[gid] for gid in groups]
         db.session.commit()
         return redirect(url_for('.index'))
-    
+
     cancel_url = url_for('.index')
 
     return render_template("users/edit.html", form=form, cancel_url=cancel_url)
+
+@bp.route("/new", methods=["GET", "POST"])
+def new():
+    form = UserForm()
+
+    query = sa.select(Group).order_by(Group.name)
+    groups = db.session.execute(query).scalars()
+
+    form.groups.choices = [
+        (g.id, g.name) for g in groups
+    ]
+
+    groups_by_id = {g.id: g for g in groups}
+
+    if form.validate_on_submit():
+        groups = form.groups.data or []
+        user = User(
+            name = form.name.data or "",
+            email = form.email.data or "",
+            admin = form.admin.data,
+            helper = form.helper.data,
+            groups = [groups_by_id[gid] for gid in groups]
+        )
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('.user', user_id=user.id))
+
+    cancel_url = url_for('.index')
+
+    return render_template("users/edit.html", form=form, cancel_url=cancel_url)
+
+@bp.route("/<int:user_id>/delete", methods=["GET", "POST"])
+def delete(user_id):
+    user = db.get_or_404(User, user_id)
+
+    form = DeleteConfirmForm()
+    if form.validate_on_submit():
+        db.session.delete(user)
+        db.session.commit()
+        flash("Event deleted")
+        return redirect(url_for('.index'))
+
+    cancel_url = url_for('.user', user_id=user.id)
+
+    return render_template("users/delete.html", form=form, user=user, cancel_url=cancel_url)
